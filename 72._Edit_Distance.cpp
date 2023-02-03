e623b8370bed3be6915cc1cba59aa6277cc2244d
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2023-02-03 22:25:19 +0000

    Add problem #72

--- 72._Edit_Distance.cpp
+++ 72._Edit_Distance.cpp
@@ -1,3 +1,19 @@
 //
 // Created by alexgershberg on 03/02/23.
 //
+
+//https://leetcode.com/problems/edit-distance/
+
+#include <string>
+
+class Solution {
+public:
+    int minDistance(std::string word1, std::string word2) {
+
+    }
+};
+
+int main()
+{
+
+}
