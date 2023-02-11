7908a1c53d39427cad80b73aeade83c158dedf08
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2023-02-11 18:28:50 +0000

    Add solution for #633

---
+++ 633._Sum_of_Square_Numbers.cpp
@@ -0,0 +1,24 @@
+//
+// Created by alexgershberg on 11/02/23.
+//
+
+#include <cmath>
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        if (c == 0 || c == 1)
+            return true;
+
+        for(int i = 0; i < sqrt(c); i++)
+        {
+            int square = i * i;
+            double first_op = c - square;
+
+            auto isPerectSquare = ceil(std::sqrt(first_op)) == floor(std::sqrt(first_op));
+            if (isPerectSquare)
+                return true;
+        }
+        return false;
+    }
+};
